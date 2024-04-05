caec18c3ed2da5c758f9149156425135c7fbf662
Ancantana <132713654+Ancantana@users.noreply.github.com>
2024-04-04 19:35:53 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,98 +3,103 @@ let textInput;
 let galleryVisible = false;
 let galleryImages = [];
 let plusButton, downloadButton;
+let selectedImage = null;
+let offsetX, offsetY;
+
+function preload() {
+  bgImage = loadImage('AFTERLIFE.png');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   textInput = createInput('');
-  textInput.position(width/2 - textInput.width/2, height/2);
-  
-  // Load images using createImg() and loadPixels()
-  bgImage = createImg('AFTERLIFE.png', () => {
-    bgImage.hide();
-    loadPixels();
+  textInput.position(width / 2 - textInput.width / 2, height / 2);
+
+  let textColorPicker = createColorPicker('#000000');
+  textColorPicker.position(20, 20);
+  textColorPicker.input(() => {
+    textInput.style('color', textColorPicker.value());
   });
-  
-  plusButton = createImg('https://assets.editor.p5js.org/user/asset/plus-button.png', () => {
-    plusButton.hide();
-    loadPixels();
+
+  let bgColorPicker = createColorPicker('#ffffff');
+  bgColorPicker.position(20, 50);
+  bgColorPicker.input(() => {
+    textInput.style('background-color', bgColorPicker.value());
   });
-  
-  downloadButton = createImg('https://assets.editor.p5js.org/user/asset/download-button.png', () => {
-    downloadButton.hide();
-    loadPixels();
-  });
-  
-  let img1 = createImg('https://assets.editor.p5js.org/user/asset/image1.png', () => {
-    img1.hide();
-    loadPixels();
-    galleryImages.push(img1);
-  });
-  
-  let img2 = createImg('https://assets.editor.p5js.org/user/asset/image2.png', () => {
-    img2.hide();
-    loadPixels();
-    galleryImages.push(img2);
-  });
-  
-  let img3 = createImg('https://assets.editor.p5js.org/user/asset/image3.png', () => {
-    img3.hide();
-    loadPixels();
-    galleryImages.push(img3);
-  });
+
+  plusButton = createImg('plusbutton.png', 'plus button');
+  plusButton.position(20, 80);
+  plusButton.size(30, 30);
+  plusButton.mousePressed(toggleGallery);
+
+  downloadButton = createImg('heaveanangel.png', 'download button');
+  downloadButton.position(20, 120);
+  downloadButton.size(30, 30);
+  downloadButton.mousePressed(() => saveCanvas('myCanvas', 'png'));
+
+  initGallery();
 }
 
 function draw() {
-  if (bgImage && bgImage.elt.complete) {
+  if (bgImage) {
     background(bgImage);
   }
-  
-  if (plusButton && plusButton.elt.complete && downloadButton && downloadButton.elt.complete) {
-    imageMode(CENTER);
-    image(plusButton, width - 50, 50, 40, 40);
-    image(downloadButton, width - 50, 100, 40, 40);
-  }
-  
-  if (galleryVisible && galleryImages.length > 0 && galleryImages.every(img => img.elt.complete)) {
+
+  if (galleryVisible) {
     drawGallery();
   }
 }
 
-function mousePressed() {
-  if (dist(mouseX, mouseY, width - 50, 50) < 20) {
-    galleryVisible = !galleryVisible;
-  }
-  
-  if (dist(mouseX, mouseY, width - 50, 100) < 20) {
-    saveCanvas('myCanvas', 'png');
-  }
+function initGallery() {
+  let imgUrls = ['original_77f8f96b25a80928f3f31b83d967fd2d.png', 'original_77f8f96b25a80928f3f31b83d967fd2d.png', 'original_77f8f96b25a80928f3f31b83d967fd2d.png'];
+  imgUrls.forEach(url => {
+    let img = createImg(url, 'image');
+    img.hide();
+    galleryImages.push(img);
+  });
+}
+
+function toggleGallery() {
+  galleryVisible = !galleryVisible;
 }
 
 function drawGallery() {
   fill(255);
-  rect(width - 200, 0, 200, height);
-  
-  for (let i = 0; i < galleryImages.length; i++) {
-    image(galleryImages[i], width - 190, i * 100 + 50, 80, 80);
-  }
+  rect(width - 330, 0, 327, 344);
+  galleryImages.forEach((img, i) => {
+    if (galleryVisible) {
+      img.show();
+      img.position(width - 320, i * 110 + 10);
+    } else {
+      img.hide();
+    }
+  });
 }
 
-function dragEnter() {
-  background(200);
-}
-
-function dragLeave() {
-  if (bgImage && bgImage.elt.complete) {
-    background(bgImage);
-  }
-}
-
-function drop(file) {
-  if (file.type === 'image') {
-    let droppedImage = createImg(file.data, () => {
-      droppedImage.hide();
-      loadPixels();
-      image(droppedImage, mouseX, mouseY, 100, 100);
+function mousePressed() {
+  if (dist(mouseX, mouseY, 20, 80) < 15) {
+    toggleGallery();
+  } else {
+    galleryImages.forEach(img => {
+      let imgX = img.position().x;
+      let imgY = img.position().y;
+      let imgW = img.width;
+      let imgH = img.height;
+      if (mouseX >= imgX && mouseX <= imgX + imgW && mouseY >= imgY && mouseY <= imgY + imgH) {
+        selectedImage = img;
+        offsetX = mouseX - imgX;
+        offsetY = mouseY - imgY;
+      }
     });
   }
+}
+
+function mouseDragged() {
+  if (selectedImage) {
+    selectedImage.position(mouseX - offsetX, mouseY - offsetY);
+  }
+}
+
+function mouseReleased() {
+  selectedImage = null;
 }
